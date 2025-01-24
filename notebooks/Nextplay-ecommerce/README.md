5fd2d9cd3d465e940e72db6e2e176e810fa38a72
DanielBorbafs <dev.danielborba@gmail.com>
2025-01-24 04:33:47 -0300

    db_insert: inserindo novos dados na tabela de games

--- notebooks/Nextplay-ecommerce/README.md
+++ notebooks/Nextplay-ecommerce/README.md
@@ -1,1 +1,2 @@
-![Modelo de dados](./imgs/databaseNextplay.png)
+## Link para visualizar as tabelas do banco
+![Nextplay_db](https://github.com/DanielBorbafs/data-science-study/blob/main/notebooks/Nextplay-ecommerce/imgs/databaseNextplay.png)
--- notebooks/Nextplay-ecommerce/SQL/inserts/estoque.sql
+++ notebooks/Nextplay-ecommerce/SQL/inserts/estoque.sql
@@ -12,7 +12,16 @@ VALUES
 (110, 'Spider-Man: Miles Morales', 'Ação', 35, 50.00, 80.00, GETDATE()),
 (111, 'Elden Ring', 'RPG', 40, 45.00, 75.00, GETDATE()),
 (112, 'Cadeira Gamer DXRacer', 'Acessórios', 5, 900.00, 1400.00, GETDATE()),
-(113, 'Monitor Gamer LG Ultragear 144Hz', 'Acessórios', 8, 1200.00, 1800.00, GETDATE());
-
+(113, 'Monitor Gamer LG Ultragear 144Hz', 'Acessórios', 8, 1200.00, 1800.00, GETDATE()),
+(114, 'Assassin’s Creed Valhalla', 'RPG', 25, 40.00, 70.00, GETDATE()),
+(115, 'Red Dead Redemption 2', 'Ação', 30, 50.00, 85.00, GETDATE()),
+(116, 'Teclado Gamer HyperX Alloy Origins', 'Acessórios', 12, 300.00, 450.00, GETDATE()),
+(117, 'Fone de Ouvido JBL Quantum 100', 'Acessórios', 18, 120.00, 250.00, GETDATE()),
+(118, 'The Legend of Zelda: Breath of the Wild', 'Ação', 15, 45.00, 80.00, GETDATE()),
+(119, 'Cyberpunk 2077', 'RPG', 20, 35.00, 70.00, GETDATE()),
+(120, 'Resident Evil 4 Remake', 'Ação', 22, 50.00, 85.00, GETDATE()),
+(121, 'Xbox Series X', 'Console', 6, 2200.00, 3500.00, GETDATE()),
+(122, 'PlayStation 5', 'Console', 4, 2500.00, 4000.00, GETDATE()),
+(123, 'Volante Logitech G29', 'Acessórios', 10, 1100.00, 1800.00, GETDATE());
 
 
