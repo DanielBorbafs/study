c17ea9d87ee1df144cb4f3bb29dc902d7a256519
DanielBorbafs <dev.danielborba@gmail.com>
2025-02-07 08:55:52 -0300

    chore: dropando projeto

--- notebooks/Nextplay-ecommerce/SQL/inserts/campanhas.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO CAMPANHAS (ID_PROMOCAO, NOME_PROMOCAO, DESCRICAO, DATA_INICIO, DATA_FIM, DESCONTO, CATEGORIAS)
-VALUES 
-(1, 'Promoção FPS Fevereiro', 'Descontos em jogos de FPS', '2025-01-01', '2025-02-15', 25, 'FPS'),
-(2, 'RPG Mania', 'Jogos de RPG com desconto especial', '2025-01-10', '2025-01-20', 30, 'RPG');
-
